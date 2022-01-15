6d3b84208afdc06630be087245aeb0e6dea4009e
Kim yeon su <rla5258@gmail.com>
2022-01-15 14:53:28 +0900

    first commit

--- js/weather.js
+++ js/weather.js
@@ -1,94 +1,41 @@
-const API_KEY = "59648956232be7bc7f53e5bebe08c609"
-const feelLikeDisplay = document.querySelector(".feel_like > span")
-const windDisplay = document.querySelector(".wind > span")
-const weatherDisplay = document.querySelector(".weather > img")
-const locationDisplay = document.querySelector(".location")
-const temperatureDisplay = document.querySelector(".temperature > span")
-const wetherSelect = document.querySelector("#weather-selecter")
-const infoSelect = document.querySelector(".info")
+const API_KEY = "59648956232be7bc7f53e5bebe08c609";
+const feelLikeDisplay = document.querySelector(".feel_like > span");
+const windDisplay = document.querySelector(".wind > span");
+const weatherDisplay = document.querySelector(".weather > img");
+const locationDisplay = document.querySelector(".location");
+const temperatureDisplay = document.querySelector(".temperature > span");
+const wetherSelect = document.querySelector("#weather-selecter");
+const infoSelect = document.querySelector(".info");
 
+wetherSelect.addEventListener("change", (e) => {
+  gerWeather(e.target.value);
+});
 
+const gerWeather = async (city = "seoul") => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
-wetherSelect.addEventListener("change",(e)=>{
-   
-    gerWeather(e.target.value)
-})
+  const response = await axios.get(url);
+  const { name, main, weather, wind } = response.data;
+  const weatherIcon = () => {
+    temperatureDisplay.innerText = transferTemp(main.temp);
+    weatherDisplay.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${weather[0].icon}.png`
+    );
+    return weatherIcon;
+  };
 
+  locationDisplay.innerText = response.data.name;
 
+  weatherIcon();
+  windDisplay.innerText = wind.speed;
+  feelLikeDisplay.innerText = transferTemp(main.feels_like);
 
-const gerWeather = async (city = 'seoul')=>{
+  console.log(response, name);
+};
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+const transferTemp = (temp) => {
+  return (temp - 273.75).toFixed(1);
+};
 
-    const response =  await axios.get(url);
-    const {name, main, weather , wind} = response.data;
-    const weatherIcon = ()=>{
-        temperatureDisplay.innerText = transferTemp(main.temp)
-        weatherDisplay.setAttribute('src',`https://openweathermap.org/img/wn/${weather[0].icon}.png`) 
-        return weatherIcon 
-    }
-
-
-
-
-    locationDisplay.innerText = response.data.name 
-    if(response.data.name === "Seoul"){
-        locationDisplay.innerText = "Sowon"         
-    }
-   if(response.data.name === "Harbin") {
-        
-        locationDisplay.innerText = "Sulhwa"         
-    }
-
-    weatherIcon()
-    windDisplay.innerText = wind.speed;
-    feelLikeDisplay.innerText = transferTemp(main.feels_like)
-    
-
-    console.log(response , name)
-
-}
-
-const transferTemp = (temp) =>{
-    return (temp - 273.75).toFixed(1)
-
-}
-
-gerWeather()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gerWeather();
